e63e9bb94028c6cc8ada0ae7821b4a4b4ecf7ec1
tzmanics <tzmanics@gmail.com>
2017-09-05 14:07:30 -0400

    GET lights & display light information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private username: string = "<username here>";
+  // ex: 2DNWwpZpUyMZ3zzaGM53HWA70kwxCu-YFTzBojG2
+  private hueApiUrl: string = `http://<Bridge IP here>/api/${this.username}/lights`;
+  // ex: 192.168.0.110
+  private lights: string[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(this.hueApiUrl)
+    .subscribe(
+      data => { this.lights = Object.keys(data).map(key => data[key]); },
+      err => { console.log('Something went wrong!'); }
+    )
+  }
 }
